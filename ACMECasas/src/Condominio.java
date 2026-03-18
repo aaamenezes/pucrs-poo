0cd620e769725e3622be4fbbd6b5364a0707dd4c
André Araujo deMenezes <57503927+aaamenezes@users.noreply.github.com>
2026-03-18 22:24:16 +0000

    Trocar for por forEach

--- ACMECasas/src/Condominio.java
+++ ACMECasas/src/Condominio.java
@@ -15,11 +15,17 @@ public class Condominio {
 
     // Operacao "R" - consultar
     public Casa consultarCasaEndereco(String endereco) {
-        for(int i=0; i<casas.size();i++) {
-            Casa aux = casas.get(i);
-            if(aux.getEndereco().equals(endereco))
-                return aux;
+        // for(int i=0; i<casas.size();i++) {
+        //     Casa aux = casas.get(i);
+        //     if(aux.getEndereco().equals(endereco))
+        //         return aux;
+        // }
+        
+        for (Casa casa : casas) {
+            if(casa.getEndereco().equals(endereco))
+                return casa;
         }
+
         return null;
     }
 
@@ -31,23 +37,35 @@ public class Condominio {
     public ArrayList<Casa> consultarTodasCasas() {
         // ArrayList auxiliar para o retorno
         ArrayList<Casa> aux = new ArrayList<>();
-        for(int i=0; i< casas.size(); i++) {
-            Casa casa = casas.get(i);
+        // for(int i=0; i< casas.size(); i++) {
+        //     Casa casa = casas.get(i);
+        //     aux.add(casa);
+        // }
+
+        for(Casa casa : casas) {
             aux.add(casa);
         }
+
         return aux;
     }
 
     public ArrayList<Casa> consultarCasasPorTamanho(int tamanho) {
-      ArrayList<Casa> casasAuxilar = new ArrayList<Casa>();
+        ArrayList<Casa> casasAuxilar = new ArrayList<Casa>();
 
-      for (int i = 0; i < casas.size(); i++) {
-        Casa casa = casas.get(i);
-        if (casa.getTamanho() == tamanho) {
-          casasAuxilar.add(casa);
+        // for (int i = 0; i < casas.size(); i++) {
+        //     Casa casa = casas.get(i);
+        //     if (casa.getTamanho() == tamanho) {
+        //     casasAuxilar.add(casa);
+        //     }
+        // }
+
+        for (Casa casa : casas) {
+            if (casa.getTamanho() == tamanho) {
+                casasAuxilar.add(casa);
+            }
         }
-      }
 
-      return casasAuxilar;
+
+        return casasAuxilar;
     }
 }
