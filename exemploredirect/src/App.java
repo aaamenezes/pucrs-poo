0ad145c50a10352680c297ff40e5e423b1de4914
André Araujo deMenezes <57503927+aaamenezes@users.noreply.github.com>
2026-03-25 23:38:17 +0000

    Entrada e saída em arquivo

--- exemploredirect/src/App.java
+++ exemploredirect/src/App.java
@@ -1,5 +1,71 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+import java.util.Locale;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    // Atributos para redirecionamento de E/S
+    private Scanner entrada = new Scanner(System.in);           // Atributo para entrada padrao (teclado)
+    private PrintStream saidaPadrao = System.out;               // Guarda a saida padrao - tela (console)
+    private final String nomeArquivoEntrada = "entrada.txt";    // Nome do arquivo de entrada de dados
+    private final String nomeArquivoSaida = "saida.txt";        // Nome do arquivo de saida de dados
+
+    // Construtor da classe de aplicacao
+    public App() {
+        redirecionaEntrada();   // Redireciona Entrada para arquivos
+        redirecionaSaida();     // Redireciona Saida para arquivos
+    }
+
+    public void executar() {
+        int numero = 0;
+
+        while (numero != -1) {
+            System.out.println("Digite um número:");
+            numero = entrada.nextInt();
+
+            if (numero != -1) System.out.println(numero);
+        }
+    }
+
+    // Redireciona Entrada de dados para arquivos em vez de teclado
+    // Chame este metodo para redirecionar a leitura de dados para arquivos
+    private void redirecionaEntrada() {
+        try {
+            BufferedReader streamEntrada = new BufferedReader(new FileReader(nomeArquivoEntrada));
+            entrada = new Scanner(streamEntrada);   // Usa como entrada um arquivo
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        Locale.setDefault(Locale.ENGLISH);   // Ajusta para ponto decimal
+        entrada.useLocale(Locale.ENGLISH);   // Ajusta para leitura para ponto decimal
+    }
+
+    // Redireciona Saida de dados para arquivos em vez da tela (terminal)
+    // Chame este metodo para redirecionar a escrita de dados para arquivos
+    private void redirecionaSaida() {
+        try {
+            PrintStream streamSaida = new PrintStream(new File(nomeArquivoSaida), Charset.forName("UTF-8"));
+            System.setOut(streamSaida);             // Usa como saida um arquivo
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        Locale.setDefault(Locale.ENGLISH);   // Ajusta para ponto decimal
+    }
+
+    // Restaura Entrada padrao para o teclado
+    // Chame este metodo para retornar a leitura de dados para o teclado
+    private void restauraEntrada() {
+        entrada = new Scanner(System.in);
+    }
+
+    // Restaura Saida padrao para a tela (terminal)
+    // Chame este metodo para retornar a escrita de dados para a tela
+    private void restauraSaida() {
+        System.setOut(saidaPadrao);
     }
 }
