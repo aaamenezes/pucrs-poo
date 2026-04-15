d02d76c2ebb325cd1b619bb32e826e527e04b229
André Araujo deMenezes <57503927+aaamenezes@users.noreply.github.com>
2026-04-15 23:44:38 +0000

    Ler arquivo datain.txt

--- T1/T1/src/Main.java
+++ T1/T1/src/Main.java
@@ -3,6 +3,6 @@ import app.ACMESpiele;
 public class Main {
     public static void main(String[] args) throws Exception {
         ACMESpiele app = new ACMESpiele();
-        app.executar();
+        app.exec();
     }
 }
--- T1/T1/src/app/ACMESpiele.java
+++ T1/T1/src/app/ACMESpiele.java
@@ -1,7 +1,35 @@
 package app;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class ACMESpiele {
-    public void executar() {
-        
+    private final String dataInFileName = "datain.txt";
+    // private final String nomeArquivoSaida = "dataout.txt";
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public ACMESpiele() {
+        createFileIn();
+    }
+
+    public void exec() {
+        String line = "";
+
+        line = scanner.nextLine();
+        line = scanner.nextLine();
+        while (!line.equals("-1")) {
+            System.out.println("próxima linha: " + line);
+        }
+    }
+
+    private void createFileIn() {
+        try {
+            BufferedReader streamEntrada = new BufferedReader(new FileReader(dataInFileName));
+            scanner = new Scanner(streamEntrada);
+        } catch (Exception error) {
+            System.out.println("Erro ao criar leitor de entrada via arquivo: " + error);
+        }
     }
 }
