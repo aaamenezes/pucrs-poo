748f86f1919f0491aaa42fc5548a73198820cac4
André Araujo deMenezes <57503927+aaamenezes@users.noreply.github.com>
2026-04-15 23:15:53 +0000

    Importar e executar ACMESpiele

--- T1/T1/src/Main.java
+++ T1/T1/src/Main.java
@@ -1,5 +1,8 @@
+import app.ACMESpiele;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        
+        ACMESpiele app = new ACMESpiele();
+        app.executar();
     }
 }
