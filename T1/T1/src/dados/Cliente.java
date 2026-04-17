1d514551a5779c9c823185487e6de3036d32c63d
André Araujo deMenezes <57503927+aaamenezes@users.noreply.github.com>
2026-04-17 21:15:39 +0000

    Criar classe Cliente

--- T1/T1/src/dados/Cliente.java
+++ T1/T1/src/dados/Cliente.java
@@ -1,5 +1,20 @@
 package dados;
 
 public abstract class Cliente {
+    private int numero;
+    private String nome;
+    private String email;
 
+    private static int contador = 0;
+
+    public Cliente(String nome, String email) {
+        this.contador++;
+        this.numero = this.contador;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String descrever() {
+        return this.numero + ";" + this.nome + ";" + this.email;
+    }
 }
