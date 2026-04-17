b96d98ac5ddc521e9bc2262244bcd2615acc2396
André Araujo deMenezes <57503927+aaamenezes@users.noreply.github.com>
2026-04-17 21:24:23 +0000

    Criar classe Corporativo

--- T1/T1/src/dados/Corporativo.java
+++ T1/T1/src/dados/Corporativo.java
@@ -1,5 +1,12 @@
 package dados;
 
 public class Corporativo extends Cliente {
+    private String cnpj;
+    private String nomeFantasia;
 
+    public Corporativo(String nome, String email, String cnpj, String nomeFantasia) {
+        super(nome, email);
+        this.cnpj = cnpj;
+        this.nomeFantasia = nomeFantasia;
+    }
 }
