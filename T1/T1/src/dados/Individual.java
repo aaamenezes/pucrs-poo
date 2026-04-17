3f5677dd153bfa085bd48c36841e70cd8856ee13
André Araujo deMenezes <57503927+aaamenezes@users.noreply.github.com>
2026-04-17 21:20:15 +0000

    Criar classe Individual

--- T1/T1/src/dados/Individual.java
+++ T1/T1/src/dados/Individual.java
@@ -1,5 +1,10 @@
 package dados;
 
 public class Individual extends Cliente {
+    private String cpf;
 
+    public Individual(String nome, String email, String cpf) {
+        super(nome, email);
+        this.cpf = cpf;
+    }
 }
