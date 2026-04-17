57f418aebc8be5a0714ca76388c23e9285fad729
André Araujo deMenezes <57503927+aaamenezes@users.noreply.github.com>
2026-04-17 21:44:34 +0000

    Fazer enum Categoria

--- T1/T1/src/dados/Categoria.java
+++ T1/T1/src/dados/Categoria.java
@@ -1,5 +1,17 @@
 package dados;
 
 public enum Categoria {
-    
+    AVENTURA("Aventura"),
+    ESTRATEGIA("Estratégia"),
+    CORRIDA("Corrida");
+
+    private String descricao;
+
+    Categoria(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
 }
