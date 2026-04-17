8356ad1403d0bb677cbbaf81f72b83bbcba12ecf
André Araujo deMenezes <57503927+aaamenezes@users.noreply.github.com>
2026-04-17 21:35:19 +0000

    Criar classe Jogo

--- T1/T1/src/dados/Jogo.java
+++ T1/T1/src/dados/Jogo.java
@@ -1,5 +1,15 @@
 package dados;
 
 public class Jogo {
+    private int codigo;
+    private String nome;
+    private int ano;
+    private double valorMinuto;
 
+    public Jogo(int codigo, String nome, int ano, double valorMinuto) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.ano = ano;
+        this.valorMinuto = valorMinuto;
+    }
 }
