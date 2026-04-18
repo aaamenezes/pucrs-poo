d71edb0c99704c87978bf74b3be8ab983b04b5da
André Menezes <1992menezes@gmail.com>
2026-04-18 13:22:40 -0300

    Add methods to app exec method

--- T1/T1/src/app/ACMESpiele.java
+++ T1/T1/src/app/ACMESpiele.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Scanner;
 
+import dados.Categoria;
+
 public class ACMESpiele {
     private final String dataInFileName = "datain.txt";
     // private final String nomeArquivoSaida = "dataout.txt";
@@ -22,6 +24,47 @@ public class ACMESpiele {
         while (!line.equals("-1")) {
             System.out.println("próxima linha: " + line);
         }
+
+        registerIndividualClients();
+        registerCorporateClientes();
+        registerGames();
+        registerContracts();
+        readGameById();
+        readGamesByCategori(Categoria.ADVENTURE);
+        updateClientNameById(1, "Novo nome");
+        clearGameContractsById(1);
+        listContracts();
+        readHighestContractValueClient();
+    }
+
+    private void registerIndividualClients() {
+    }
+
+    private void registerCorporateClientes() {
+    }
+
+    private void registerGames() {
+    }
+
+    private void registerContracts() {
+    }
+
+    private void readGameById() {
+    }
+
+    private void readGamesByCategori(Categoria category) {
+    }
+
+    private void updateClientNameById(int id, String newName) {
+    }
+
+    private void clearGameContractsById(int id) {
+    }
+
+    private void listContracts() {
+    }
+
+    private void readHighestContractValueClient() {
     }
 
     private void createFileIn() {
