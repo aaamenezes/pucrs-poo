7a92e9e5daa23782f11a3d311bc12503294215a1
André Menezes <1992menezes@gmail.com>
2026-05-03 13:23:26 -0300

    Remover atributo descricao do enum

--- T1/T1/src/dados/Categoria.java
+++ T1/T1/src/dados/Categoria.java
@@ -1,17 +1,7 @@
 package dados;
 
 public enum Categoria {
-    AVENTURA("Aventura"),
-    ESTRATEGIA("Estratégia"),
-    CORRIDA("Corrida");
-
-    private String descricao;
-
-    private Categoria(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getDescricao() {
-        return this.descricao;
-    }
+    AVENTURA,
+    ESTRATEGIA,
+    CORRIDA;
 }
